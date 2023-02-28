90c8a92c889e6c5e7872a2146db0833850db08d0
Keny Pau Vas <keny_pauccarima@usmp.pe>
2023-02-28 18:52:50 -0500

    ...

--- src/app/admin/users/user-list/user-list.component.ts
+++ src/app/admin/users/user-list/user-list.component.ts
@@ -5,23 +5,33 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
-  templateUrl: './user-list.component.html'
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.css']
 
 })
 export class UserListComponent implements OnInit {
 
   users:User[]=[];
-  id!:number;
+
+  displayedColumns:string [] = ['firstName','lastName','fullName','email','role','actions'];
   constructor(private userService:UserService,private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.userService.paginate()
-    .subscribe(users=>{
-      console.log('users',users);
-      this.users =users;
-    })
-    this.id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.getUsers();
+    //this.id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
   }
 
-
+  getUsers(){
+    this.userService.paginate()
+    .subscribe(users=>{
+      this.users =users;
+    });
+  }
+  deleteUser(user:User){
+    if (confirm('Estas seguro de eliminar este usuario?')) {
+      this.userService.delete(user.id!).subscribe(() => {
+        this.getUsers();
+      });
+    }
+  }
 }
