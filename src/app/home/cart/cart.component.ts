3dca71e8e71df81819dc8d20ac64c2e0a68435e2
Keny Pau Vas <keny_pauccarima@usmp.pe>
2023-07-06 19:22:44 -0500

    ...

--- src/app/home/cart/cart.component.ts
+++ src/app/home/cart/cart.component.ts
@@ -17,6 +17,7 @@ export class CartComponent implements OnInit{
     private activatedRoute : ActivatedRoute,
     private router:Router
     ) {}
+
   ngOnInit(): void {
     const token = this.activatedRoute.snapshot.queryParamMap.get('token');
 
